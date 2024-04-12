018f2fa0016bee9ea233a9713dfbe9a2572406fd
shivakshisharma <shivakshisharma2000@gmail.com>
2024-04-12 16:56:56 +0530

    Add template.html and modify app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import seaborn as sns
+from sklearn.metrics import r2_score, recall_score
+print("Hello World")
+j=0
+for i in range(100):
+    j=j+i
+print(j)
