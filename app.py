f341b38038a77f1554c16758b78535d90ac4d4cd
Gaurav Patil <69918345+patilgaurav1499@users.noreply.github.com>
2024-04-12 17:48:38 +0530

    World to Gauarv

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib as plt
 import seaborn as sns
 from sklearn.metrics import r2_score, recall_score
-print("Hello World")
+print("Hello Guarav")
 j=0
 for i in range(100):
     j=j+i
